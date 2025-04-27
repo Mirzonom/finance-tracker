b7de86af6bbd949aa29a17ae02d8a8bbf75033a6
Inom Dev <inommirzosharipov4@gmail.com>
2025-04-27 23:01:35 +0500

    feat(home): add balance, income & expenses

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {TransactionService} from '../../core/services/transaction.service';
+import {DecimalPipe} from '@angular/common';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [
+    DecimalPipe,
+    RouterLink
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  balance = 0;
+  income = 0;
+  expense = 0;
 
+  constructor(private transactionService: TransactionService) {
+  }
+
+  ngOnInit(): void {
+    this.transactionService.transactions$.subscribe(transactions => {
+      this.income = transactions
+        .filter(t => t.type === 'income')
+        .reduce((sum, t) => sum + t.amount, 0);
+
+      this.expense = transactions
+        .filter(t => t.type === 'expense')
+        .reduce((sum, t) => sum + t.amount, 0);
+
+      this.balance = this.income - this.expense;
+    });
+  }
 }
