7e413226608b3c191b7c8d68cffba1fae9ec7c5d
Inom Dev <inommirzosharipov4@gmail.com>
2025-04-27 23:12:08 +0500

    feat(statistics): income and expense statistics

--- src/app/features/statistics/statistics.component.ts
+++ src/app/features/statistics/statistics.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {TransactionService} from '../../core/services/transaction.service';
+import {DecimalPipe} from '@angular/common';
 
 @Component({
   selector: 'app-statistics',
-  imports: [],
+  imports: [
+    DecimalPipe
+  ],
   templateUrl: './statistics.component.html',
   styleUrl: './statistics.component.scss'
 })
 export class StatisticsComponent {
+  income = 0;
+  expense = 0;
 
+  constructor(private transactionService: TransactionService) {
+  }
+
+  ngOnInit(): void {
+    this.transactionService.transactions$.subscribe(transactions => {
+      this.income = transactions
+        .filter(t => t.type === 'income')
+        .reduce((sum, t) => sum + t.amount, 0);
+
+      this.expense = transactions
+        .filter(t => t.type === 'expense')
+        .reduce((sum, t) => sum + t.amount, 0);
+    });
+  }
 }
