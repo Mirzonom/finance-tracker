1e729cb84ad7a3e2c4a8c087003d42f31bfafe70
Inom Dev <inommirzosharipov4@gmail.com>
2025-04-27 23:10:06 +0500

    feat(transaction-history): transaction items

--- src/app/features/transaction-history/transaction-history.component.ts
+++ src/app/features/transaction-history/transaction-history.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Transaction} from '../../core/models/transaction.model';
+import {TransactionService} from '../../core/services/transaction.service';
+import {NgForOf} from '@angular/common';
+import {TransactionItemComponent} from '../../shared/components/transaction-item/transaction-item.component';
 
 @Component({
   selector: 'app-transaction-history',
-  imports: [],
+  imports: [
+    NgForOf,
+    TransactionItemComponent
+  ],
   templateUrl: './transaction-history.component.html',
   styleUrl: './transaction-history.component.scss'
 })
-export class TransactionHistoryComponent {
+export class TransactionHistoryComponent implements OnInit {
+  transactions: Transaction[] = [];
 
+  constructor(private transactionService: TransactionService) {
+  }
+
+  ngOnInit(): void {
+    this.transactionService.transactions$.subscribe(transactions => {
+      this.transactions = transactions;
+    });
+  }
 }
